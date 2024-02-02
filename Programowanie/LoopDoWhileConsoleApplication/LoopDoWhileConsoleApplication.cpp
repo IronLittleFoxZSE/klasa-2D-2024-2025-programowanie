9b74ad740ac514503f5cf7cdbb0faaecb941d427
g <g@g.pl>
2024-02-02 10:36:03 +0100

    koniec lekcji

---
+++ Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
@@ -0,0 +1,45 @@
+﻿// LoopDoWhileConsoleApplication.cpp 
+#include <iostream>
+
+//Napisz program, który wczyta od użytkownika liczbę dodatnią
+void task1()
+{
+	int number;
+
+	/*
+	std::cout << "Podaj liczbę dodatnią\n";
+	std::cin >> number;
+	if (number < 0)
+	{
+		std::cout << "Podaj liczbę dodatnią\n";
+		std::cin >> number;
+		if (number < 0)
+		{
+			std::cout << "Podaj liczbę dodatnią\n";
+			std::cin >> number;
+			if (number < 0)
+			{
+				std::cout << "Podaj liczbę dodatnią\n";
+				std::cin >> number;
+				if (number < 0)
+				{
+					//....
+				}
+			}
+		}
+	}
+	*/
+
+	do
+	{
+		std::cout << "Podaj liczbę dodatnią\n";
+		std::cin >> number;
+	} while (number < 0);
+
+	std::cout << "Podano liczbę " << number << "\n";
+}
+
+int main()
+{
+	task1();
+}
