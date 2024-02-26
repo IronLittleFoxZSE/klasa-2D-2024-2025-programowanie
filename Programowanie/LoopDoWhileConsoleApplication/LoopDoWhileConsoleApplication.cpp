bb7e99bc90bb9fc2e6e49ae60313643b5111cc4d
g <g@g.pl>
2024-02-26 10:04:36 +0100

    koniec lekcji

--- Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
+++ Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
@@ -84,7 +84,70 @@ void task2()
 	std::cout << "Gratulacje!!!\nZgałeś liczbę\n";
 }
 
+//Napisz program wyświetlający liczby całkowite z przedziału <0,x> (wartość x podaje użytkownik)
+void task3()
+{
+	long long upperRange;
+	std::cout << "Podaj górny zakres:\n";
+	std::cin >> upperRange;
+
+	/*
+	std::cout << "0, ";
+	if (upperRange >= 1)
+	{
+		std::cout << "1, ";
+		if (upperRange >= 2)
+		{
+			std::cout << "2, ";
+			if (upperRange >= 3)
+			{
+				//...
+			}
+		}
+	}
+	*/
+
+	int number = 0;
+	do
+	{
+		std::cout << number << ", ";
+		number++;
+	} while (upperRange >= number);
+}
+
+//Napisz program, który policzy sumę cyfr podanej przez użytkownika liczby.
+void task4()
+{
+	int number;
+	do
+	{
+		std::cout << "Podaj liczbę dodatnią\n";
+		std::cin >> number;
+	} while (number < 0);
+
+	int tmpNumber;
+
+	do
+	{
+		/*
+		tmpNumber = -1;
+		do
+		{
+			tmpNumber++;
+		} while ((number - tmpNumber) % 10 != 0);
+		
+		std::cout << tmpNumber << ", ";
+		*/
+
+		int rest = number % 10;
+		std::cout << rest << ", ";
+
+		number = number / 10;
+
+	} while (number > 0);
+}
+
 int main()
 {
-	task2();
+	task4();
 }
