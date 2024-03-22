e78ee6a4636201e349b0483dd36c19c81bd7867f
g <g@g.pl>
2024-03-22 15:00:19 +0100

    koniec lekcji

---
+++ Programowanie/CharactersConsoleApplication/CharactersConsoleApplication.cpp
@@ -0,0 +1,25 @@
+﻿// CharactersConsoleApplication.cpp 
+#include <iostream>
+
+//Napisz program który pobierze znak od użytkownika i wyświeli go.
+void task1()
+{
+    char characterFromUser;
+    std::cout << "Podaj jeden znak:\n";
+    std::cin >> characterFromUser;
+
+    std::cout << "Podano: " << characterFromUser << "\n";
+
+    characterFromUser = characterFromUser + 1;
+
+    std::cout << "Podano: " << characterFromUser << "\n";
+
+    characterFromUser = 97;
+    characterFromUser = 'a';
+    characterFromUser = 'a' + 1; // 'b'
+}
+
+int main()
+{
+    task1();
+}
