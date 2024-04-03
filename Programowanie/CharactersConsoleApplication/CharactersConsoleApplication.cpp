974ad45fa02a1101b7f5641d7f48f6003a7f6c7b
g <g@g.pl>
2024-04-03 10:40:49 +0200

    koniec lekcji

--- Programowanie/CharactersConsoleApplication/CharactersConsoleApplication.cpp
+++ Programowanie/CharactersConsoleApplication/CharactersConsoleApplication.cpp
@@ -4,22 +4,104 @@
 //Napisz program który pobierze znak od użytkownika i wyświeli go.
 void task1()
 {
-    char characterFromUser;
-    std::cout << "Podaj jeden znak:\n";
-    std::cin >> characterFromUser;
+	char characterFromUser;
+	std::cout << "Podaj jeden znak:\n";
+	std::cin >> characterFromUser;
 
-    std::cout << "Podano: " << characterFromUser << "\n";
+	std::cout << "Podano: " << characterFromUser << "\n";
 
-    characterFromUser = characterFromUser + 1;
+	characterFromUser = characterFromUser + 1;
 
-    std::cout << "Podano: " << characterFromUser << "\n";
+	std::cout << "Podano: " << characterFromUser << "\n";
 
-    characterFromUser = 97;
-    characterFromUser = 'a';
-    characterFromUser = 'a' + 1; // 'b'
+	characterFromUser = 97;
+	characterFromUser = 'a';
+	characterFromUser = 'a' + 1; // 'b'
+}
+
+// Napisz program, który wczyta znak z klawiatury i sprawdzi czy jest to mała litera alfabetu.
+void task2()
+{
+	char characterFromUser;
+	std::cout << "Podaj jeden znak:\n";
+	std::cin >> characterFromUser;
+
+	//wersja 1
+	if (characterFromUser == 'a'
+		|| characterFromUser == 'b'
+		|| characterFromUser == 'c'
+		|| characterFromUser == 'd'
+		//...
+		|| characterFromUser == 'z')
+	{
+		std::cout << "Podałes małą literę alfabetu\n";
+	}
+	else
+	{
+		std::cout << "Podałeś inny znak\n";
+	}
+
+	//wersja 2
+	if (characterFromUser >= 97
+		&& characterFromUser <= 122)
+	{
+		std::cout << "Podałes małą literę alfabetu\n";
+	}
+	else
+	{
+		std::cout << "Podałeś inny znak\n";
+	}
+
+	//wersja 3
+	if (characterFromUser >= 'a'
+		&& characterFromUser <= 'z')
+	{
+		std::cout << "Podałes małą literę alfabetu\n";
+	}
+	else
+	{
+		std::cout << "Podałeś inny znak\n";
+	}
+}
+
+//Napisz program, który poprosi cie o twoje imię i cię przywita.
+void task3()
+{
+	std::string userName;
+	std::cout << "Podaj swoje imię\n";
+	std::cin >> userName;
+
+	std::cout << "Witaj " << userName << " tutaj.\n";
+
+	userName = "To jest nowy tekst";
+}
+
+//Program sprawdzający czy podane hasło jest poprawne 
+// (np. jeśli hasło jest "abc123", 
+// program powinien wyświetlić "hasło poprawne",
+//  jeśli jest inne, powinien wyświetlić "hasło niepoprawne").
+void task4()
+{
+	std::string password;
+	std::cout << "Podaj hasło\n";
+	std::cin >> password;
+
+	if (password == "abc123")
+	{
+		std::cout << "Hasło poprawne\n";
+	}
+	else
+		std::cout << "Hasło niepoprawne\n";
+}
+
+//Napisz program, który wczyta łańcuch znaków i policzy ile jest małych liter 'a'.
+void task5()
+{
+
 }
 
 int main()
 {
-    task1();
+	setlocale(LC_CTYPE, "polish");
+	task4();
 }
