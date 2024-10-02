04b8e679a4f7470766246f155d2ee5c350d10525
f <f@f.pl>
2024-10-02 09:00:47 +0200

    pierwszy program obiektowy

---
+++ Programowanie/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+class bankAccount
+{
+	double balance; // saldo
+	std::string owner; //w³aœciciel
+	std::string currency; //waluta
+
+};
+
+void accountInformation(bankAccount& account)
+{
+	std::cout << "Informacja o koncie bankowym.\n";
+	std::cout << "W³aœciciel: " << account.owner << "\n";
+	std::cout << "Saldo: " << account.balance << " " << account.currency << "\n";
+}
+
+void depositToAccount(bankAccount& account, double amount)
+{
+	amount = abs(amount);
+	account.balance = account.balance + amount;
+}
+
+bool widthdrawalFromAccount(bankAccount& account, double amount)
+{
+	amount = abs(amount);
+	if (account.balance - amount >= 0)
+	{
+		account.balance = account.balance - amount;
+		return true;
+	}
+	return false;
+}
+
+void transferBetweenAcounts(bankAccount& sourceAccount, bankAccount& targetAccount, double amount)
+{
+	if (widthdrawalFromAccount(sourceAccount, amount) == true)
+		depositToAccount(targetAccount, amount);
+}
+
+int main()
+{
+	bankAccount firstAccount;
+	firstAccount.balance = 10000;
+	firstAccount.currency = "z³";
+	firstAccount.owner = "Jan Kowalski";
+
+	accountInformation(firstAccount);
+
+	bankAccount secondAccount;
+	secondAccount.balance = 15000;
+	secondAccount.currency = "z³";
+	secondAccount.owner = "Ewa Nowak";
+
+	accountInformation(secondAccount);
+
+	depositToAccount(firstAccount, -14.50);
+	accountInformation(firstAccount);
+
+	widthdrawalFromAccount(firstAccount, 14.50);
+	accountInformation(firstAccount);
+
+	transferBetweenAcounts(secondAccount, firstAccount, 1000);
+	accountInformation(firstAccount);
+	accountInformation(secondAccount);
+}
