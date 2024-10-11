23e06de55fe53771d0638f777765f482a5755297
f <f@f.pl>
2024-10-11 10:44:28 +0200

    koniec lekcji

--- Programowanie/ReadAndWriteOnFilesConsoleApplication/ReadAndWriteOnFilesConsoleApplication.cpp
+++ Programowanie/ReadAndWriteOnFilesConsoleApplication/ReadAndWriteOnFilesConsoleApplication.cpp
@@ -6,33 +6,34 @@
 //Napisz funkcjê zapisuj¹c¹ dan¹ do pliku tekstowego
 void task1()
 {
-    std::string name;
-    int age;
+	std::string name;
+	int age;
 
-    std::cout << "Podaj imie\n";
-    std::cin >> name;
+	std::cout << "Podaj imie\n";
+	std::cin >> name;
 
-    std::cout << "Podaj wiek\n";
-    std::cin >> age;
+	std::cout << "Podaj wiek\n";
+	std::cin >> age;
 
-    std::ofstream writeInFile;
-    writeInFile.open("c:\\dane.txt");
+	std::ofstream writeInFile;
+	writeInFile.open("c:\\dane.txt", std::ios::app);
 
-    if (writeInFile.is_open() == true)
-    {
-        //praca na pliku
-        writeInFile << name;
-        writeInFile << "\n";
-        writeInFile << age;
+	if (writeInFile.is_open() == true)
+	{
+		//praca na pliku
+		writeInFile << name;
+		writeInFile << "\n";
+		writeInFile.flush();
+		writeInFile << age << "\n";
 
-        writeInFile.close();
-    }
-    else
-        std::cout << "B³¹d otwarcia pliku\n";
+		writeInFile.close();
+	}
+	else
+		std::cout << "B³¹d otwarcia pliku\n";
 }
 
 
 int main()
 {
-    task1();
+	task1();
 }
