f1343f2a2cd1221790e162ab0471e00bdbfcb3c2
f <f@f.pl>
2024-11-27 08:38:29 +0100

    dodanie zadania

--- Programowanie/InheritanceConsoleApplication/InheritanceConsoleApplication.cpp
+++ Programowanie/InheritanceConsoleApplication/InheritanceConsoleApplication.cpp
@@ -5,6 +5,10 @@
 
 int main()
 {
+    //Napisz klasy które opisz¹ figury geometryczne: 
+    // trapez, romb, równoleg³obok, prostok¹t i kwadrat.
+    // Zdefiniuj podstawowe metody.
+
     //BankAccount b;
     //b.AccountInformation();
 
