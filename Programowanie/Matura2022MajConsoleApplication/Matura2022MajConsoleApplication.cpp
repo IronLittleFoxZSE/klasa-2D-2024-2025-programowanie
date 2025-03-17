55146e374689dcc68eec9c7bce915f99c2d6423f
f <f@f.pl>
2025-03-17 14:23:20 +0100

    dodanie odczytu z pliku

---
+++ Programowanie/Matura2022MajConsoleApplication/Matura2022MajConsoleApplication.cpp
@@ -0,0 +1,38 @@
+// Matura2022MajConsoleApplication.cpp :
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+/*
+1. Kopiujemy pliki txt do folderu z projektem
+2. PPM na pliku txt i wybieramy Properties.
+3. Zmieniamy Item type na "Copy to"
+*/
+
+int main()
+{
+    std::ifstream file("przyklad.txt");
+
+	/*
+    int numbers[200];
+
+	for (int i = 0; i < 200; i++)
+	{
+		file >> numbers[i];
+	}*/
+
+	std::vector<int> numbers;
+	
+	int n;
+	while (file >> n)
+	{
+		numbers.push_back(n);
+	}
+
+
+	std::cout << "Odczytane liczby:\n";
+	for (int i = 0; i < numbers.size(); i++)
+	{
+		std::cout << numbers[i] << ", ";
+	}
+}
