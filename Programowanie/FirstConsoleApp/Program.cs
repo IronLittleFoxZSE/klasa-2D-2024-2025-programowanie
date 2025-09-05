4a87b24c7792c86c3fd7774a8367495f9796ef2b
f <f@f.pl>
2025-09-05 10:41:06 +0200

    dodanie pierwszego projektu koniec lekcji

---
+++ Programowanie/FirstConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.Write("Hello, World!\n");
+Console.WriteLine("Hello, World!");
+
+string name = "Jan";
+string surname = "Kowalski";
+Console.WriteLine("Witaj " + name + " " + surname + " tutaj!!!");
